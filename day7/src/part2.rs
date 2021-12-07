ec89e33daefe40868804440ba5c102252865daea
OverHash <46231745+OverHash@users.noreply.github.com>
2021-12-07 19:18:33 +1300

    Document day 7 part 2 solution
    Labels the sum of cumulative integers formula in the code so that it is
    more clear what this math is doing and where it came from

--- day7/src/part2.rs
+++ day7/src/part2.rs
@@ -11,6 +11,7 @@ pub fn solve(input: &str) -> u32 {
                 .iter()
                 .map(|other_crab| {
                     let crab_distance = (other_crab - crab).abs();
+                    // sum of cumulative integers formula
                     (crab_distance * (crab_distance + 1) / 2) as u32
                 })
                 .sum()
